dca2f88c2c5073d3b63b1911333a395ece266737
Hieu-PhamAnh <hieupa.b19cn254@stu.ptit.edu.vn>
2023-11-30 22:02:46 +0700

    update

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Param,
+  Post,
+  Get,
+} from '@nestjs/common';
 import { UserService } from './user.service';
+import { User } from './user.schema';
+import mongoose from 'mongoose';
 
 @Controller('user')
 export class UserController {
@@ -8,5 +17,11 @@ export class UserController {
   @Post('create')
   async createUser(@Body() user: any) {
     return this.userService.create(user);
+  }
+
+  @Get('profile/:id')
+  async getUser(@Param('id') id: string): Promise<Partial<User> & any> {
+    if (!mongoose.Types.ObjectId.isValid(id))
+      throw new BadRequestException('Invalid user id');
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { Model } from 'mongoose';
+import { FilterQuery, Model, QueryOptions } from 'mongoose';
 import { User } from './user.schema';
 import { InjectModel } from '@nestjs/mongoose';
 
@@ -10,5 +10,32 @@ export class UserService {
   create(user: Partial<User>): Promise<User> {
     const createdUser = new this.userModel({ ...user, joinedAt: new Date() });
     return createdUser.save();
+  }
+
+  find(
+    filter: FilterQuery<User>,
+    returnFields?: string[],
+    queryOptions?: QueryOptions,
+  ): Promise<User[]> {
+    return this.userModel.find(filter, returnFields, queryOptions).exec();
+  }
+
+  findOne(
+    filter: FilterQuery<User>,
+    returnFields?: string[],
+    queryOptions?: QueryOptions,
+  ): Promise<User> {
+    return this.userModel.findOne(filter, returnFields, queryOptions).exec();
+  }
+
+  async getUserFriends(
+    filter: FilterQuery<User>,
+    returnFields: string[] = ['_id'],
+  ): Promise<User[]> {
+    const result = await this.userModel
+      .findOne(filter, 'friends')
+      .populate('friends', returnFields)
+      .exec();
+    return result.friends;
   }
 }
